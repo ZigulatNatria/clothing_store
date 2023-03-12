7332df8cc27dee80cc4b12ee930d1e83184ed68b
ZigulatNatria <96946885+ZigulatNatria@users.noreply.github.com>
2023-03-12 23:43:25 +0500

    Добавлена функция представление для отображения продукта по категории, добавлены ссылки перехода с категории в продукты по категории, добавлен класс-представление для отображения детальной информации о продукте, добавлена ссылка на переход к детальной информации о продукте

--- backend/store/shop/urls.py
+++ backend/store/shop/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProductsListView, CategoryProductListView
+from .views import CategoryProductListView, ProductDetail, product_categories
 
 
 urlpatterns =[
-    path('products/', ProductsListView.as_view(), name='products'),
+    # path('products/<int:pk>/', ProductsListView.as_view(), name='products'),
+    path('product/<int:pk>/', ProductDetail.as_view(), name='product'),
+    path('products/<int:category_product_id>/', product_categories, name='products'),
     path('categories/', CategoryProductListView.as_view(), name='categories'),
 ]
--- backend/store/shop/views.py
+++ backend/store/shop/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from .models import Product, CategoryProduct
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-class ProductsListView(ListView):
-    model = Product
-    template_name = 'products.html'
-    context_object_name = 'products'
+# class ProductsListView(ListView):
+#     model = Product
+#     template_name = 'products_categories.html'
+#     context_object_name = 'pro'
+#     queryset = Product.objects.all()
+#
+#     def get_object(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         id = self.get(**kwargs)
+#         category = CategoryProduct.objects.get(pk=id)
+#         # products = Product.objects.filter(category_product=category.id)
+#         context['products'] = Product.objects.filter(category_product=category.id)
+#         return context
+
+
+def product_categories(request, category_product_id):
+    category = CategoryProduct.objects.get(pk=category_product_id)
+    products = Product.objects.filter(category_product=category.id)
+    context = {'products': products}
+    return render(request, 'products_categories.html', context)
+
+
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
     queryset = Product.objects.all()
 
 
