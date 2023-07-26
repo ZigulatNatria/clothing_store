1fa4c923a58f8d55187f22d550186e4ecf242a13
ZigulatNatria <vachrameev.oleg@yandex.ru>
2023-07-27 01:25:51 +0500

    Настройка админки

--- backend/store/shop/admin.py
+++ backend/store/shop/admin.py
@@ -3,18 +3,10 @@ from .models import ClientCategory, ClothingCategories, CategoryProduct, Stock, 
     Collection, Product, News, Color, Size, Favorites
 from django.utils.safestring import mark_safe
 
-# Register your models here.
-admin.site.register(ClientCategory)
-admin.site.register(ClothingCategories)
-admin.site.register(CategoryProduct)
-admin.site.register(Stock)
-admin.site.register(Collection)
-# admin.site.register(Product)
-admin.site.register(News)
-admin.site.register(Color)
-admin.site.register(Size)
-admin.site.register(Favorites)
 
+admin.site.register(ClientCategory)
+admin.site.register(Stock)
+admin.site.register(Favorites)
 
 
 @admin.register(Product)
@@ -24,21 +16,122 @@ class ProductAdmin(admin.ModelAdmin):
         'get_image',
         'vendor_code',
         'price',
-        # 'stock_availability',
-        # 'description',
-        # 'compound',
-        # 'size_on_model',
-        # 'collection',
+        'collection',
         'category_product',
     ]
+
     list_filter = [
         'name',
         'vendor_code',
+        'category_product',
+        'collection',
     ]
 
     def get_image(self, obj):
-        return mark_safe(f'<img src={obj.photo_1.url} width="50" height="60"')
+        return mark_safe(f'<img src={obj.photo_1.url} width="50" height="70"')
 
     get_image.short_description = 'Изображение'
     # list_editable = ['price', 'name']
     # prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+    ]
+
+    list_filter = [
+        'name',
+    ]
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = [
+        'name_color',
+        'get_image',
+        'vendor_code',
+    ]
+
+    list_filter = [
+        'name_color',
+        'vendor_code',
+    ]
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.photo_1.url} width="50" height="70"')
+
+    get_image.short_description = 'Изображение'
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = [
+        'size',
+        'vendor_code',
+    ]
+
+    list_filter = [
+        'size',
+        'vendor_code',
+    ]
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = [
+        'header',
+        'get_image',
+        'author',
+        'date_add',
+    ]
+
+    list_filter = [
+        'header',
+        'author',
+        'date_add',
+    ]
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="70" height="50"')
+
+    get_image.short_description = 'Изображение'
+
+
+@admin.register(CategoryProduct)
+class CategoryProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'get_image',
+        'client_category',
+    ]
+
+    list_filter = [
+        'name',
+        'client_category',
+    ]
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.photo.url} width="50" height="70"')
+
+    get_image.short_description = 'Изображение'
+
+
+@admin.register(ClothingCategories)
+class ClothingCategoriesAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'get_image',
+        'client_category',
+    ]
+
+    list_filter = [
+        'name',
+        'client_category',
+    ]
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.photo.url} width="50" height="70"')
+
+    get_image.short_description = 'Изображение'
--- backend/store/shop/models.py
+++ backend/store/shop/models.py
@@ -3,34 +3,37 @@ from django.contrib.auth import get_user_model
 
 
 class ClientCategory(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name='Название категории')
+    name = models.CharField(max_length=100, unique=True, verbose_name='название категории (М/Ж)')
 
     class Meta:
         verbose_name = 'Категория клиентов (М Ж)'
+        verbose_name_plural = 'категории клиентов (М Ж)'
 
     def __str__(self):
         return self.name
 
 
 class ClothingCategories(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name='Название раздела категории одежды')
+    name = models.CharField(max_length=100, unique=True, verbose_name='название раздела категории одежды')
     client_category = models.ForeignKey('ClientCategory', on_delete=models.CASCADE)
-    photo = models.ImageField(verbose_name='Фото категрии', null=True, blank=True)
+    photo = models.ImageField(verbose_name='фото категрии', null=True, blank=True)
 
     class Meta:
-        verbose_name = 'Раздел категории одежды'
+        verbose_name = 'раздел категории одежды'
+        verbose_name_plural = 'разделы категорий одежды'
 
     def __str__(self):
         return self.name
 
 
 class CategoryProduct(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name='Категория одежды')
-    photo = models.ImageField(verbose_name='Фото категрии', null=True, blank=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name='категория одежды')
+    photo = models.ImageField(verbose_name='фото категрии', null=True, blank=True)
     client_category = models.ForeignKey('ClothingCategories', on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = 'Категория одежды'
+        verbose_name = 'категория одежды'
+        verbose_name_plural = 'категории одежды'
 
     def __str__(self):
         return self.name
@@ -40,17 +43,19 @@ class Stock(models.Model):
     quantity = models.CharField(max_length=50, unique=True, verbose_name='количество на складе')
 
     class Meta:
-        verbose_name = 'Наличие на складе'
+        verbose_name = 'наличие на складе'
+        verbose_name_plural = 'наличие на складах'
 
     def __str__(self):
         return self.quantity
 
 
 class Collection(models.Model):
-    name = models.CharField(max_length=150, unique=True, verbose_name='Название коллекции')
+    name = models.CharField(max_length=150, unique=True, verbose_name='название коллекции')
 
     class Meta:
-        verbose_name = 'Коллекция'
+        verbose_name = 'коллекция'
+        verbose_name_plural = 'коллекции'
 
     def __str__(self):
         return self.name
@@ -83,67 +88,88 @@ class Collection(models.Model):
 
 
 class Color(models.Model):
-    vendor_code = models.CharField(max_length=100, verbose_name='Артикул товара')
-    name_color = models.CharField(max_length=100, verbose_name='Название цвета')
-    photo_1 = models.ImageField(verbose_name='Фото 1')
-    photo_2 = models.ImageField(verbose_name='Фото 2')
-    photo_3 = models.ImageField(verbose_name='Фото 3')
-    photo_4 = models.ImageField(verbose_name='Фото 4')
-    photo_5 = models.ImageField(verbose_name='Фото 5')
+    vendor_code = models.CharField(max_length=100, verbose_name='артикул товара')
+    name_color = models.CharField(max_length=100, verbose_name='название цвета')
+    photo_1 = models.ImageField(verbose_name='фото 1')
+    photo_2 = models.ImageField(verbose_name='фото 2')
+    photo_3 = models.ImageField(verbose_name='фото 3')
+    photo_4 = models.ImageField(verbose_name='фото 4')
+    photo_5 = models.ImageField(verbose_name='фото 5')
 
     class Meta:
-        verbose_name = 'Цвет'
+        verbose_name = 'цвет'
+        verbose_name_plural = 'цвета'
 
     def __str__(self):
         return '{}'.format(self.name_color) + 'цвет товара с артикулом' + ' ' + '{}'.format(self.vendor_code)
 
 
 class Size(models.Model):
-    vendor_code = models.CharField(max_length=100, verbose_name='Артикул товара')
-    size = models.CharField(max_length=100, unique=True, verbose_name='Размер')
+    vendor_code = models.CharField(max_length=100, verbose_name='артикул товара')
+    size = models.CharField(max_length=100, unique=True, verbose_name='размер')
 
     class Meta:
-        verbose_name = 'Размер'
+        verbose_name = 'размер'
+        verbose_name_plural = 'размеры'
 
     def __str__(self):
         return 'Размер товара с артикулом' + ' ' + '{}'.format(self.vendor_code)
 
 
 class Product(models.Model):
-    photo_1 = models.ImageField(verbose_name='Фото 1')
-    photo_2 = models.ImageField(verbose_name='Фото 2')
-    photo_3 = models.ImageField(verbose_name='Фото 3')
-    photo_4 = models.ImageField(verbose_name='Фото 4')
-    photo_5 = models.ImageField(verbose_name='Фото 5')
-    name = models.CharField(max_length=100, verbose_name='Наименование товара')
-    vendor_code = models.CharField(max_length=100, unique=True, verbose_name='Артикул')
+    photo_1 = models.ImageField(verbose_name='фото 1')
+    photo_2 = models.ImageField(verbose_name='фото 2')
+    photo_3 = models.ImageField(verbose_name='фото 3')
+    photo_4 = models.ImageField(verbose_name='фото 4')
+    photo_5 = models.ImageField(verbose_name='фото 5')
+    name = models.CharField(max_length=100, verbose_name='наименование товара')
+    vendor_code = models.CharField(max_length=100, unique=True, verbose_name='артикул')
     price = models.FloatField(verbose_name='цена')
-    colors = models.ManyToManyField(Color, verbose_name='Цвета', blank=True, null=True)
-    size = models.ManyToManyField(Size, verbose_name='Размеры', blank=True, null=True)
-    stock_availability = models.ForeignKey('Stock', on_delete=models.CASCADE, verbose_name='Наличие на складе', null=True, blank=True)
-    description = models.TextField(verbose_name='Описание товара')
-    compound = models.TextField(verbose_name='Состав и уход')
-    size_on_model = models.TextField(verbose_name='Размер модели\размер на модели')
-    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, verbose_name='Коллекция', null=True, blank=True)
-    category_product = models.ForeignKey('CategoryProduct', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Категория одежды')
-    #TODO возможно докинуть поля связанные с категриями ClothingCategories, ClientCategory
+    colors = models.ManyToManyField(Color, verbose_name='цвета', blank=True)
+    size = models.ManyToManyField(Size, verbose_name='размеры', blank=True)
+    stock_availability = models.ForeignKey(
+        'Stock',
+        on_delete=models.CASCADE,
+        verbose_name='наличие на складе',
+        null=True,
+        blank=True
+    )
+    description = models.TextField(verbose_name='описание товара')
+    compound = models.TextField(verbose_name='состав и уход')
+    size_on_model = models.TextField(verbose_name='размер модели\размер на модели')
+    collection = models.ForeignKey(
+        Collection,
+        on_delete=models.CASCADE,
+        verbose_name='коллекция',
+        null=True,
+        blank=True
+    )
+    category_product = models.ForeignKey(
+        'CategoryProduct',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name='категория одежды'
+    )
 
     class Meta:
-        verbose_name = 'Товар'
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
 
     def __str__(self):
         return self.name
 
 
 class News(models.Model):
-    header = models.CharField(max_length=100, verbose_name='Заголовок новости')
+    header = models.CharField(max_length=100, verbose_name='заголовок новости')
     text = models.TextField()
-    author = models.CharField(max_length=50, verbose_name='Автор новости')
+    author = models.CharField(max_length=50, verbose_name='автор новости')
     date_add = models.DateField(auto_now=True)
     image = models.ImageField()
 
     class Meta:
-        verbose_name = 'Новости'
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
         ordering = ['-id']
 
     def __str__(self):
@@ -151,11 +177,13 @@ class News(models.Model):
 
 
 class Favorites(models.Model):
-    user = models.ForeignKey('auth.User', related_name='Пользователь', on_delete=models.CASCADE)
-    product = models.ForeignKey('Product', related_name='Товар', on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', related_name='пользователь', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', related_name='товар', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
+        verbose_name = 'избранное'
+        verbose_name_plural = 'избранные'
         indexes = [
             models.Index(fields=['-created']),
         ]
