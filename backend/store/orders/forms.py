05cc285586230648e4f83a6e4e55f4bce37ddaad
ZigulatNatria <vachrameev.oleg@yandex.ru>
2023-08-17 17:25:41 +0500

    Отредактирована форма для заполнения заказа

--- backend/store/orders/forms.py
+++ backend/store/orders/forms.py
@@ -13,16 +13,70 @@ class OrderCreateForm(forms.ModelForm):
         )
     )
 
+    city = forms.CharField(
+        label='Город',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+
+    postal_code = forms.CharField(
+        label='Индекс',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+
+    address = forms.CharField(
+        label='Адрес',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+
+    name = forms.CharField(
+        label='ФИО',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+
+    email = forms.CharField(
+        label='Эл.почта',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+
+    phone = forms.CharField(
+        label='Телефон',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+
     class Meta:
         model = Order
         fields = [
             # 'country',
-            'city',
-            'postal_code',
-            'address',
-            'name',
-            'email',
-            'phone',
+            # 'city',
+            # 'postal_code',
+            # 'address',
+            # 'name',
+            # 'email',
+            # 'phone',
         ]
 
 
