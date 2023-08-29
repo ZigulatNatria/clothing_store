8c34a47ed5d6dce16015685e5332649bb8d32cc9
ZigulatNatria <vachrameev.oleg@yandex.ru>
2023-08-29 22:52:20 +0500

    Настроена форма оформления заказа

--- backend/store/orders/forms.py
+++ backend/store/orders/forms.py
@@ -4,108 +4,103 @@ from .models import Order
 
 class OrderCreateForm(forms.ModelForm):
     # Order.authorUser = request.user.username TODO доделать привязку автора
-    country = forms.CharField(
-        label='Страна',
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
-                "placeholder": 'Страна',
-            }
-        )
-    )
-
-    city = forms.CharField(
-        label='Город',
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
-                "placeholder": 'Город',
-            }
-        )
-    )
-
-    postal_code = forms.CharField(
-        label='Индекс',
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
-                "placeholder": 'Индекс',
-            }
-        )
-    )
-
-    address = forms.CharField(
-        label='Адрес',
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
-                "placeholder": 'Адрес',
-            }
-        )
-    )
-
-    name = forms.CharField(
-        label='ФИО',
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
-                "placeholder": 'ФИО',
-            }
-        )
-    )
-
-    email = forms.CharField(
-        label='Эл.почта',
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
-                "placeholder": 'Email',
-
-            }
-        )
-    )
-
-    phone = forms.CharField(
-        label='Телефон',
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
-                "placeholder": 'Телефон',
-            }
-        )
-    )
-
-    DELIVERY = [
-        ('Почтой России', 'Почтой России'),
-        ('Транспортной компанией', 'Транспортной компанией'),
-        ('Самовывоз', 'Самовывоз'),
-    ]
-
-    delivery = forms.MultipleChoiceField(
-        required=False,
-        widget=forms.CheckboxSelectMultiple,
-        choices=DELIVERY,
-    )
 
     class Meta:
         model = Order
         fields = [
-            # 'country',
-            # 'city',
-            # 'postal_code',
-            # 'address',
-            # 'name',
-            # 'email',
-            # 'phone',
+            'country',
+            'city',
+            'postal_code',
+            'address',
+            'name',
+            'email',
+            'phone',
+            'delivery'
         ]
 
+        widgets = {
+            'country': forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
+                    "placeholder": 'Страна',
+                }
+            ),
+
+            'city': forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
+                    "placeholder": 'Город',
+                }
+            ),
+
+            'postal_code': forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
+                    "placeholder": 'Индекс',
+                }
+            ),
+
+            'address': forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
+                    "placeholder": 'Адресс',
+                }
+            ),
+
+            'name': forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
+                    "placeholder": 'ФИО',
+                }
+            ),
+
+            'email': forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
+                    "placeholder": 'Email',
+                }
+            ),
+
+            'phone': forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
+                    "placeholder": 'Телефон',
+                }
+            ),
+
+            'delivery': forms.RadioSelect
+
+        }
+
+        # country = forms.CharField(
+        #     label='Страна',
+        #     widget=forms.TextInput(
+        #         attrs={
+        #             "class": "form-control",
+        #             "style": "border-left: 0; border-top: 0; border-right: 0; padding-left: 0; border-radius: 0",
+        #             "placeholder": 'Страна',
+        #         }
+        #     )
+        # )
+
+        # DELIVERY = [
+        #     ('Почтой России', 'Почтой России'),
+        #     ('Транспортной компанией', 'Транспортной компанией'),
+        #     ('Самовывоз', 'Самовывоз'),
+        # ]
+        #
+        # delivery = forms.MultipleChoiceField(
+        #     required=False,
+        #     widget=forms.CheckboxSelectMultiple,
+        #     choices=DELIVERY,
+        # )
 
 class OrderUpdateForm(forms.ModelForm):
 
